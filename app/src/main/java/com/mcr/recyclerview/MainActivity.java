0b9a15cb53bc71e5d0b9510210abb24d64c4c781
Richi <richi_mcr@hotmail.com>
2020-05-20 03:22:49 -0500

    Se agrego un boton para agregar nombres manualmente

--- app/src/main/java/com/mcr/recyclerview/MainActivity.java
+++ app/src/main/java/com/mcr/recyclerview/MainActivity.java
@@ -14,43 +14,5 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btActivacion;
-    RecyclerView recyclerNombres;
-    RecyclerView.Adapter mAdapter;
-    RecyclerView.LayoutManager recyclerManager;
-    List<String> nombres;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        nombres = new ArrayList<>();
-        nombres.add("Ricardo");
-        nombres.add("Alejandra");
-        recyclerNombres = (RecyclerView) findViewById(R.id.recyclerNombres);
-        recyclerManager=new LinearLayoutManager(getApplicationContext());
-
-        mAdapter = new Adapter(nombres, R.layout.recycler_latyout, new Adapter.OnItemClickListener() {
-            @Override
-            public void onItemClickListener(String nombre, int posicion) {
-                Toast.makeText(getApplicationContext(),nombre+" esta en la posicion "+posicion,Toast.LENGTH_SHORT).show();
-            }
-        });
-        recyclerNombres.setLayoutManager(recyclerManager);
-        recyclerNombres.setAdapter(mAdapter);
-
-         btActivacion = (Button) findViewById(R.id.btActivar);
-         btActivacion.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Toast.makeText(getApplicationContext(),"Hola soy un boton",Toast.LENGTH_SHORT).show();
-             }
-         });
-
-
-    }
-
-
-
 
 }
